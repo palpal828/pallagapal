f57687e783ab2dec97d499d5c7874651410fbd6f
Pallaga Pál <115153237+palpal828@users.noreply.github.com>
2025-09-22 11:04:15 +0200

    qw

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <BrowserRouter>
+      <BrowserRouter> 
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
